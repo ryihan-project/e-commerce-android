82dfc39be663471572fa0d3623f3a4f3fea0fa15
raihan <ryihan55@gmail.com>
2021-05-20 05:55:06 +0600

    Update ProductReview.dart

--- ManagerApp/lib/models/ProductReview.dart
+++ ManagerApp/lib/models/ProductReview.dart
@@ -8,4 +8,11 @@ class ProductReview{
   int id,rating,orderId,productId,userId;
   String review;
   DateTime createdAt;
+  User user;
+  Product product;
+  ProductReview(this.id, this.rating, this.orderId, this.productId, this.userId,
+      this.review,this.createdAt,this.user,this.product);
+
+  ProductReview.dummy(this.id, this.rating, this.orderId, this.productId, this.userId,
+      this.review);
 }
