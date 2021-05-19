19348f3a4398c6476ad918f7cb243e46926c157a
raihan <ryihan55@gmail.com>
2021-05-20 05:55:11 +0600

    Update ProductReview.dart

--- ManagerApp/lib/models/ProductReview.dart
+++ ManagerApp/lib/models/ProductReview.dart
@@ -15,4 +15,11 @@ class ProductReview{
 
   ProductReview.dummy(this.id, this.rating, this.orderId, this.productId, this.userId,
       this.review);
+  static ProductReview fromJson(Map<String, dynamic> jsonObject) {
+
+    int id = int.parse(jsonObject['id'].toString());
+    int rating = int.parse(jsonObject['rating'].toString());
+    int productId = int.parse(jsonObject['product_id'].toString());
+    int orderId = int.parse(jsonObject['order_id'].toString());
+    int userId = int.parse(jsonObject['user_id'].toString());
 }
