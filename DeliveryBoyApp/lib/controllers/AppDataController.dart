bb4deaaa5483df4951a5829524ef10f460ede78c
raihan <ryihan55@gmail.com>
2021-05-28 10:33:04 +0600

    Create AppDataController.dart

---
+++ DeliveryBoyApp/lib/controllers/AppDataController.dart
@@ -0,0 +1,6 @@
+import 'dart:convert';
+
+import 'package:DeliveryBoyApp/api/api_util.dart';
+import 'package:DeliveryBoyApp/models/AppData.dart';
+import 'package:DeliveryBoyApp/models/MyResponse.dart';
+import 'package:DeliveryBoyApp/utils/InternetUtils.dart';
