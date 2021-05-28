e6c084840188fc1cf794632d9c4ac87e59504c79
raihan <ryihan55@gmail.com>
2021-05-28 10:33:13 +0600

    Update AppDataController.dart

--- DeliveryBoyApp/lib/controllers/AppDataController.dart
+++ DeliveryBoyApp/lib/controllers/AppDataController.dart
@@ -4,3 +4,9 @@ import 'package:DeliveryBoyApp/api/api_util.dart';
 import 'package:DeliveryBoyApp/models/AppData.dart';
 import 'package:DeliveryBoyApp/models/MyResponse.dart';
 import 'package:DeliveryBoyApp/utils/InternetUtils.dart';
+
+import 'package:http/http.dart' as http;
+
+import 'AuthController.dart';
+
+class AppDataController {
