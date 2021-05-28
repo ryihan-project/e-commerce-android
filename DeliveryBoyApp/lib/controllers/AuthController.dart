1c036696726eb4abab99abde8bf156b2b84e6187
raihan <ryihan55@gmail.com>
2021-05-28 10:33:47 +0600

    Create AuthController.dart

---
+++ DeliveryBoyApp/lib/controllers/AuthController.dart
@@ -0,0 +1,13 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:DeliveryBoyApp/AppTheme.dart';
+import 'package:DeliveryBoyApp/api/api_util.dart';
+import 'package:DeliveryBoyApp/models/DeliveryBoy.dart';
+import 'package:DeliveryBoyApp/models/MyResponse.dart';
+import 'package:DeliveryBoyApp/services/PushNotificationsManager.dart';
+import 'package:DeliveryBoyApp/utils/InternetUtils.dart';
+import 'package:DeliveryBoyApp/utils/SizeConfig.dart';
+import 'package:DeliveryBoyApp/utils/TextUtils.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:shared_preferences/shared_preferences.dart';
